1812877b33c5d9e7f9df82334d9fab454a320347
Aner Lucero <4rgento@gmail.com>
2018-12-06 16:20:24 -0300

    Day 5 part 2

--- src/Day05.hs
+++ src/Day05.hs
@@ -7,6 +7,7 @@ module Day05 (
 import qualified System.IO as SysIO
 import Data.Char (toUpper, isUpper, isLower)
 import Data.Function (on)
+import qualified Data.List as DL
 
 sameType :: Char -> Char -> Bool
 sameType = (==) `on` toUpper
@@ -32,3 +33,14 @@ solve_1 =
 	SysIO.withFile "inputs/day05" SysIO.ReadMode $ \input_fh ->
 		(lines <$> SysIO.hGetContents input_fh) >>= \file_lines -> do
 		print $ fmap (length . reduce) file_lines
+
+solve_2 :: IO ()
+solve_2 = 
+	SysIO.withFile "inputs/day05" SysIO.ReadMode $ \input_fh ->
+		(lines <$> SysIO.hGetContents input_fh) >>= \file_lines -> do
+		mapM_ (\line -> do
+			let worst_char = DL.minimumBy (compare `on` (\c ->
+				length $ reduce $ filter (not . sameType c) line )) ['a'..'z']
+			print worst_char
+			print $ (length . reduce . filter (not . sameType worst_char)) line
+			) file_lines
